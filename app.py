75a7bd3288cb87936fbec30fb2a4deb422e5e347
Ghostik-gh <54578428+Ghostik-gh@users.noreply.github.com>
2022-04-29 13:48:11 +0300

    to linux

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
-from importlib.resources import path
 import os
 import shutil
-from flask import Flask, flash, request, redirect, url_for, render_template, after_this_request
+from flask import Flask, request, redirect, url_for, render_template, Response
 from werkzeug.utils import secure_filename
-from yaml import load
 import Yolov5_DeepSort_Pytorch.track
 from threading import Thread 
 import threading
+import gi
 
 
 app = Flask(__name__)
@@ -15,8 +14,7 @@ app = Flask(__name__)
 UPLOAD_FOLDER = 'uploads/'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# th = Thread(target=Yolov5_DeepSort_Pytorch.track.start, args=(filepath, )) # Запускаем разметку видео
-# th.start()
+
 
 @app.route('/', methods=['POST'])
 def upload_file_POST():
@@ -47,7 +45,19 @@ def upload_file_POST():
 def upload_file_GET():
     return render_template('index.html')
 
-
+"""
+####################
+# YIELDS
+def step_loads(file):
+    yield render_template('loading.html')
+    filepath = f"{app.config['UPLOAD_FOLDER']}/{file}"
+    th = Thread(target=Yolov5_DeepSort_Pytorch.track.start, args=(filepath, )) # Запускаем разметку видео
+    th.start()
+    th.join()
+    yield redirect(url_for('download', file = file))
+    
+return Response(step_loads(file)) # Добавить в loading
+"""
 @app.route('/load/<file>', methods=['GET', 'POST'])
 def loading(file):
     # @after_this_request
@@ -55,11 +65,13 @@ def loading(file):
     #     # Запускает Треккинг видео
     #     # Переносим файл в static
     #     return responce
-    return render_template('loading.html')
+    return redirect(url_for('download', file = file))
+    # return render_template('loading.html')
 
 
 @app.route('/download/<file>', methods=['GET', 'POST'])
 def download(file):
+
     ###########################################################################
     # Переносим размеченный файл 
     try:
@@ -68,6 +80,7 @@ def download(file):
         get_files = os.listdir(file_source)    
         for g in get_files:
             shutil.move(file_source + g, file_destination)
+            print(g, "Transfered by", file_destination, "And rdy for using")
     except:
         print("File already exists")
     ###########################################################################
@@ -80,6 +93,29 @@ def download(file):
             shutil.rmtree(delete_folder + g)
     except:
         print("Failed Delete")
+    ###########################################################################
+    # # Gstreamer here :))))))))    
+    # # Это, если вы используете deepsort в файле object_tracker.py
+    # # Советую изучить как строить пайплайны в gstreamer и впринципе все эти теги для пайплайна
+    # # pipe_out как раз является пайплайном
+    # pipe_out = 'appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=700 speed-preset=superfast ! decodebin ! autovideoconvert ! theoraenc ! oggmux ! tcpserversink host=127.0.0.1 port=8080'
+
+
+    # try:
+    #     vid = cv2.VideoCapture("filesrc location=./data/video/hype.mp4 ! decodebin ! videoconvert ! appsink", cv2.CAP_GSTREAMER)
+    # except Exception as e:
+    #     print(e)
+
+    # out = cv2.VideoWriter(pipe_out, 0, 30, (int(vid.get(cv2.CAP_PROP_FRAME_WIDTH)), int(vid.get(cv2.CAP_PROP_FRAME_HEIGHT))), True)
+
+    # # Всё запускается из файла server.py:
+    # # Но только фронт у нас в отдельном докер контейнере лежит
+    # from absl import app
+    # import object_tracker as object_tracker
+
+    # object_tracker.set_flags()
+
+    # app.run(object_tracker.main)
     ###########################################################################
     return render_template('videoplayer.html', filename=file)
 
