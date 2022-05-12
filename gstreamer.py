1f74a61f338805d6aa83a628c7c151693da1ef29
ghostik-gh <ghostlyhinter@gmail.com>
2022-05-12 21:00:34 +0300

    файл с тестами gstreamera

--- gstreamer.py
+++ gstreamer.py
@@ -13,8 +13,7 @@ Gst.init()
 main_loop = GLib.MainLoop()
 thread = Thread(target=main_loop.run)
 thread.start()
-
-pipeline = Gst.parse_launch("filesrc location=./static/tempfile.mp4 ! videoconvert ! x264enc tune=zerolatency bitrate=700 speed-preset=superfast ! decodebin ! autovideoconvert ! theoraenc ! oggmux ! tcpserversink host=127.0.0.1 port=8080")
+pipeline = Gst.parse_launch("filesrc location=./output/tempfile.mp4 ! decodebin ! autovideoconvert ! autovideosink")
 pipeline.set_state(Gst.State.PLAYING)
 try:
     while True:
@@ -25,6 +24,7 @@ except KeyboardInterrupt:
 pipeline.set_state(Gst.State.NULL)
 main_loop.quit()
 
+# pipeline = Gst.parse_launch("filesrc location=./static/tempfile.mp4 ! videoconvert ! x264enc tune=zerolatency bitrate=700 speed-preset=superfast ! decodebin ! autovideoconvert ! theoraenc ! oggmux ! tcpserversink host=127.0.0.1 port=8080")
 # pipe_out = 'appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=700 speed-preset=superfast ! decodebin ! autovideoconvert ! theoraenc ! oggmux ! tcpserversink host=127.0.0.1 port=8080'
 # pipeline = Gst.parse_launch("audiotestsrc wave=saw freq=205 volume=0.1 ! autoaudiosink")
 # pipeline = Gst.parse_launch("filesrc location=./static/tempfile.mp4 ! videoconvert ! x264enc tune=zerolatency bitrate=700 speed-preset=superfast ! decodebin ! autovideoconvert ! theoraenc ! oggmux ! filesink location=./output.mp4")
