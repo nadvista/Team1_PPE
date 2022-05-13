88dd6f76e9e29d028d7dce9eb7dfd2f536bca72c
ghostik-gh <ghostlyhinter@gmail.com>
2022-05-13 19:13:58 +0300

    запускает видео из output в новом окне

--- app.py
+++ app.py
@@ -86,15 +86,15 @@ def download():
     main_loop = GLib.MainLoop()
     thread = Thread(target=main_loop.run)
     thread.start()
-    pipeline = Gst.parse_launch("filesrc location=./output/tempfile.mp4 ! decodebin ! autovideoconvert ! shout2send ip=127.0.0.1 port=5000")
+    pipeline = Gst.parse_launch("filesrc location=./output/tempfile.mp4 ! decodebin ! autovideoconvert ! autovideosink")
     pipeline.set_state(Gst.State.PLAYING)
     # try:
     #     while True:
     #         sleep(0.1)
     # except KeyboardInterrupt:
     #     pass
-    pipeline.set_state(Gst.State.NULL)
-    main_loop.quit()
+    # pipeline.set_state(Gst.State.NULL)
+    # main_loop.quit()
     # pipe_out = 'appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=700 speed-preset=superfast ! decodebin ! autovideoconvert ! theoraenc ! oggmux ! tcpserversink host=127.0.0.1 port=8080'
     # pipe_out = 'filesrc location=static/tempfile.mp4 ! videoconvert ! x264enc tune=zerolatency bitrate=700 speed-preset=superfast ! decodebin ! autovideoconvert ! theoraenc ! oggmux ! tcpserversink host=127.0.0.1 port=5000'
     # pipeline = Gst.parse_launch("filesrc location=./static/tempfile.mp4 ! videoconvert ! x264enc tune=zerolatency bitrate=700 speed-preset=superfast ! decodebin ! autovideoconvert ! theoraenc ! oggmux ! filesink location=./output.mp4")
--- gstreamer.py
+++ gstreamer.py
@@ -15,14 +15,14 @@ thread = Thread(target=main_loop.run)
 thread.start()
 pipeline = Gst.parse_launch("filesrc location=./uploads/tempfile.mp4 ! decodebin ! autovideoconvert ! autovideosink")
 pipeline.set_state(Gst.State.PLAYING)
-try:
-    while True:
-        sleep(0.1)
-except KeyboardInterrupt:
-    pass
+# try:
+#     while True:
+#         sleep(0.1)
+# except KeyboardInterrupt:
+#     pass
 
-pipeline.set_state(Gst.State.NULL)
-main_loop.quit()
+# pipeline.set_state(Gst.State.NULL)
+# main_loop.quit()
 
 # pipeline = Gst.parse_launch("filesrc location=./static/tempfile.mp4 ! videoconvert ! x264enc tune=zerolatency bitrate=700 speed-preset=superfast ! decodebin ! autovideoconvert ! theoraenc ! oggmux ! tcpserversink host=127.0.0.1 port=8080")
 # pipe_out = 'appsrc ! videoconvert ! x264enc tune=zerolatency bitrate=700 speed-preset=superfast ! decodebin ! autovideoconvert ! theoraenc ! oggmux ! tcpserversink host=127.0.0.1 port=8080'
