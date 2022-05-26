40504985d1e4a4629be2e1007a7e9e4ac01f17a7
EgorKirnosov <kirnosov.eg@yandex.ru>
2022-05-26 19:05:42 +0300

    Downloading: добавил кнпоку загрузки бд

--- download.py
+++ download.py
@@ -4,10 +4,17 @@ from flask import Blueprint, send_file, render_template, current_app
 download_bp = Blueprint('download', __name__,
                         template_folder='templates')
 
-@download_bp.route('/downloadfile/')
-def download_file():
+@download_bp.route('/downloadvideo/')
+def download_video():
     path = os.path.join(current_app.root_path,
                         current_app.config['DOWNLOAD_FOLDER'], "tempfile.mp4")
+    return send_file(path, as_attachment=True)
+
+
+@download_bp.route('/downloaddb/')
+def download_db():
+    path = os.path.join(current_app.root_path,
+                        current_app.config['DOWNLOAD_FOLDER'], "ИМЯФАЙЛА!!!")
     return send_file(path, as_attachment=True)
 
 @download_bp.route('/download/')
