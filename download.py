48be010363f2f0a6178b02915b8d51260545067e
ghostik-gh <ghostlyhinter@gmail.com>
2022-05-26 20:47:06 +0300

    Самый просто плеер в Gstreamer поверх браузера

--- download.py
+++ download.py
@@ -1,5 +1,11 @@
 import os
+from threading import Thread
 from flask import Blueprint, send_file, render_template, current_app
+# Среда разаработки выделяет все связанное с gstreamer предупреждениями
+# Однако все работает
+import gi
+gi.require_version("Gst", "1.0")
+from gi.repository import Gst, GLib
 
 download_bp = Blueprint('download', __name__,
                         template_folder='templates')
@@ -21,5 +27,11 @@ def download_db():
 def download_page():
     ###########################################################################
     # # Gstreamer here :))))))))
+    Gst.init()
+    main_loop = GLib.MainLoop()
+    thread = Thread(target=main_loop.run)
+    thread.start()
+    pipeline = Gst.parse_launch("filesrc location=./static/tempfile.mp4 ! decodebin ! autovideoconvert ! autovideosink")
+    pipeline.set_state(Gst.State.PLAYING)
     ###########################################################################
     return render_template('videoplayer.html', filename=f"{current_app.config['DOWNLOAD_FOLDER']}/tempfile.mp4")
